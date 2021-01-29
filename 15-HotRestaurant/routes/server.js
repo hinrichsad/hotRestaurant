5a1ff334a5fed31e99071925c223020063d9119f
Young Ji Kim <youjmik@gmail.com>
2021-01-28 20:46:01 -0500

    updating my htmls

--- 15-HotRestaurant/routes/server.js
+++ 15-HotRestaurant/routes/server.js
@@ -14,53 +14,50 @@ app.use(express.json());
 
 
 
-const characters = [
+const newTable = [
   {
-    routeName: 'yoda',
-    name: 'Yoda',
-    role: 'Jedi Master',
-    age: 900,
-    forcePoints: 2000,
+    name: "Andy",
+    contact: "12345",
+    email: "hinrichsad@gmail.com",
+    uniqueId: 1,
   },
   {
-    routeName: 'darthmaul',
-    name: 'Darth Maul',
-    role: 'Sith Lord',
-    age: 200,
-    forcePoints: 1200,
+    name: "Chris",
+    contact: "54321",
+    email: "chris@gmail.com",
+    uniqueId: 2,
   },
   {
-    routeName: 'obiwankenobi',
-    name: 'Obi Wan Kenobi',
-    role: 'Jedi Master',
-    age: 55,
-    forcePoints: 1350,
+    name: "Jeezy",
+    contact: "11111111",
+    email: "test@gmail.com",
+    uniqueId: 3,
   },
 ];
 
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
 app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
 
 // Displays all characters
-app.get('/api/characters', (req, res) => res.json(characters));
+app.get('/makeres', (req, res) => res.json(newTable));
 
 // Displays a single character, or returns false
-app.get('/api/characters/:character', (req, res) => {
-  const chosen = req.params.character;
+app.get('/makeres/:res', (req, res) => {
+  const rezzy = req.params.reservation;
 
-  console.log(chosen);
+  console.log(rezzy);
 
   /* Check each character routeName and see if the same as "chosen"
    If the statement is true, send the character back as JSON,
    otherwise tell the user no character was found */
 
-  for (let i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+  for (let i = 0; i < newTable.length; i++) {
+    if (chosen === newTable[i].name) {
+      return res.json(newTable[i]);
     }
   }
 
@@ -68,18 +65,18 @@ app.get('/api/characters/:character', (req, res) => {
 });
 
 // Create New Characters - takes in JSON input
-app.post('/api/characters', (req, res) => {
+app.post('/makeres', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  const newCharacter = req.body;
+  const newTable = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-  console.log(newCharacter);
+  newTable.name = newTable.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newTable);
 
-  characters.push(newCharacter);
-  res.json(newCharacter);
+  characters.push(newTable);
+  res.json(newTable);
 });
 
 // Starts the server to begin listening
